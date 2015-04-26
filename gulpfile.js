5f613619875143381190f0fa2e56a801ff4c85e5
Greg Trowbridge <gjtrowbridge@gmail.com>
2015-04-26 00:04:03 -0700

    Add basic gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
 var gulp = require('gulp');
 
+var jshint = require('gulp-jshint');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+
+gulp.task('lint', function() {
+  return gulp.src('src/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/index.js')
+      .pipe(browserify({
+        insertGlobals: true,
+        debug: !gulp.env.production
+      }))
+      .pipe(rename('bundle.js'))
+      .pipe(gulp.dest('./public/js'));
+})
+
 gulp.task('default', function() {
-  // place code for your default task here
+  gulp.run('lint', 'scripts');
 });
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+var React = require('react');
+var DisplayBox = require('./DisplayBox.js');
+var EditBox = require('./EditBox.js');
+var Page = require('./Page.js');
+
+
