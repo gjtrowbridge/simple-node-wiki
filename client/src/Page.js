4e6b4827bbc2d8456699c3cf1cfb6527416f614a
Greg Trowbridge <gjtrowbridge@gmail.com>
2015-05-09 23:14:05 -0700

    Gulp is too much work, switched back to npm build type stuff

--- client/src/Page.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var Page = React.createClass({
-  displayName: 'Page',
-  render: function() {
-    return React.createElement('div', null, 'Hello ', this.props.name);
-  }
-});
-
-module.exports = Page;
