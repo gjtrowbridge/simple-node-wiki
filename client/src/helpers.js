f5999e1eb2a087ab2ce5a0e8dd3402aa8046b4d5
Greg Trowbridge <gjtrowbridge@gmail.com>
2015-05-25 13:33:02 -0700

    Add helpers file

---
+++ client/src/helpers.js
@@ -0,0 +1,21 @@
+
+var helpers = {
+  convertStringToPath: function(str) {
+    var result = '';
+    for (var i=0; i<str.length; i++) {
+      var ch = str.charAt(i);
+      if (ch === ch.toLowerCase()) {
+        result += ch;
+      } else {
+        result += '-' + ch.toLowerCase();
+      }
+    }
+    result = result.replace(/\W+/g, '-');
+    result = result.replace(/^-/, '');
+    result = result.replace(/-$/, '');
+    result = result.replace(/-(2,)/g, '');
+    return result;
+  }
+};
+
+module.exports = helpers;
