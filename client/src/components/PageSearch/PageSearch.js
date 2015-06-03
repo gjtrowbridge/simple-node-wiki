230149462cd7b627e31678fce7384860059d517f
Greg Trowbridge <gjtrowbridge@gmail.com>
2015-06-02 20:59:21 -0700

    Add filter to PageSearch class

--- client/src/components/PageSearch/PageSearch.js
+++ client/src/components/PageSearch/PageSearch.js
@@ -3,14 +3,51 @@ var d = React.DOM;
 
 var PageSearch = React.createClass({
   displayName: 'PageSearch',
+  getInitialState: function() {
+    return {
+      value: ''
+    };
+  },
+  getDefaultProps: function() {
+    return {
+      pages: [{title: 'hello'}, {title: 'hel'}],
+      maxResults: 5
+    }
+  },
+  onChange: function(e) {
+    this.setState({
+      value: e.target.value
+    });
+  },
+  matchingPages: function() {
+    var me = this;
+    if (me.state.value === '') {
+      return [];
+    } else {
+      return this.props.pages.filter(function(page) {
+        return page.title.toLowerCase().indexOf(me.state.value) >= 0;
+      }).slice(0, this.props.maxResults);
+    }
+  },
+  renderSearchResults: function() {
+    var matchingPages = this.matchingPages();
+    console.log(matchingPages);
+  },
   render: function() {
-    return d.input(      
+    var searchResults = this.renderSearchResults();
+    return d.div(
       {
-        type: 'search',
         className: 'page-search horizontal-center',
-        placeholder: 'Search Pages'
-      }
-    );
+      },
+      d.input(      
+        {
+          type: 'search',
+          className: 'horizontal-center',
+          placeholder: 'Search Pages',
+          onChange: this.onChange
+        }
+      )
+    )
   }
 });
 
