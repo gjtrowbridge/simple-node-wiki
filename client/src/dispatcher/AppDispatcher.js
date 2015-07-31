3060e3e28837753575b7763dd7aa4fd216ab0652
Greg Trowbridge <gjtrowbridge@gmail.com>
2015-07-31 13:29:31 -0700

    Refactor dispatcher

--- client/src/dispatcher/AppDispatcher.js
+++ client/src/dispatcher/AppDispatcher.js
@@ -1,3 +1,34 @@
 var Dispatcher = require('flux').Dispatcher;
+var assign = require('object-assign');
 
-module.exports = new Dispatcher();
+var flux = new Dispatcher();
+
+module.exports = {
+  register: function(callback) {
+    return flux.register(callback);
+  },
+  dispatch: function(type, action) {
+    var info = assign({type: type}, action);
+    flux.dispatch(info);
+  },
+  dispatchAsync: function(promise, types, action) {
+    requestType = types.request;
+    successType = types.success;
+    failureType = types.failure;
+
+    // Dispatch an event that we are making a request
+    this.dispatch(requestType, action);
+
+    // Dispatch an event after the request is complete
+    promise.then(
+      function(responseSuccess) {
+        var info = assign({}, action, responseSuccess);
+        this.dispatch(successType, info);
+      },
+      function(responseFailure) {
+        var info = assign({}, action, responseFailure);
+        this.dispatch(failureType, info);
+      }
+    );
+  }
+};
---
+++ client/src/utils/WikiUtils.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  // Executes the specified HTTP request and returns
+  // a promise that resolves to the response
+  requestViaHttpAndReturnPromise: function(url, verb, data) {
+
+  }
+
+};
