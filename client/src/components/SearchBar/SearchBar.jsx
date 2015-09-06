601e15ee6e7cfd159846940e5be5c8c769e066e7
Greg Trowbridge <gjtrowbridge@gmail.com>
2015-09-05 23:40:41 -0700

    Add search results updating

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,35 @@
 var React = require('react');
+var PageListStore = require('../../stores/PageListStore.js');
+var WikiPageActionCreators = require('../../actions/WikiPageActionCreators.js');
+var shared = require('../../../../shared/shared.js');
 
 var SearchBar = React.createClass({
-  componentWillMount: function() {
-
+  getInitialState: function() {
+    return this.getStateFromStores();
+  },
+  componentDidMount: function() {
+    PageListStore.addChangeListener(this._onChange);
+  },
+  componentWillUnmount: function() {
+    PageListStore.removeChangeListener(this._onChange);
+  },
+  getStateFromStores: function() {
+    return {
+      searchResults: PageListStore.getPageList(shared.constants.SEARCH) || []
+    };
+  },
+  _onChange: function() {
+    this.setState(this.getStateFromStores());
+  },
+  search: function(e) {
+    WikiPageActionCreators.searchPages({
+      searchTerm: e.target.value
+    });
   },
   render: function() {
+    console.log(this.state);
     return (
-      <input type="search" placeholder="Enter Search Term" />
+      <input onChange={this.search} type="search" placeholder="Enter Search Term" />
     );
   }
 });
