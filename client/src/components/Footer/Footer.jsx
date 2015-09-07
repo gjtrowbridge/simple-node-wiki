a8b8ffc63ae344c3a7dd0e6a90603a542cfae3c1
Greg Trowbridge <gjtrowbridge@gmail.com>
2015-09-07 13:11:12 -0700

    Style footer and add link

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var Footer = React.createClass({
   render: function() {
     return (
-      <footer id="footer">
-        Created by Greg Trowbridge
+      <footer id="footer" className="fill-and-center">
+        <p>Created by
+          <a target="_blank" href="https://github.com/gjtrowbridge"> Greg Trowbridge</a>
+        </p>
       </footer>
     );
   }
