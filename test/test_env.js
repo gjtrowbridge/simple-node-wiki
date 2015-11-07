11745d2b7ca9974b06482f057ecfa20646f85d33
Greg Trowbridge <gjtrowbridge@gmail.com>
2015-11-06 22:55:37 -0800

    Configure package.json to also create test database

--- test/test_env.js
+++ test/test_env.js
@@ -4,3 +4,7 @@ test('Test code runs with NODE_ENV=test', function(t) {
   t.plan(1);
   t.equal('test', process.env.NODE_ENV)
 });
+
+if (process.env.NODE_ENV === 'test') {
+  // Delete test database
+}
