58cff8b8302da970f7eef145da364f79bef471c3
Gregory Trowbridge <gregt@uber.com>
2018-08-10 17:59:53 -0700

    More attempts to get DB to migrate.

--- server/database/models/index.js
+++ server/database/models/index.js
@@ -1,29 +1,16 @@
 "use strict";
 
-const fs        = require("fs");
-const path      = require("path");
 const Sequelize = require("sequelize");
-const basename  = path.basename(module.filename);
 const env       = process.env.NODE_ENV || "development";
 const config    = require(__dirname + '/../config/config.json')[env];
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
-const db        = {};
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== basename);
-  })
-  .forEach(function(file) {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-Object.keys(db).forEach(function(modelName) {
-  if ("associate" in db[modelName]) {
-    db[modelName].associate(db);
-  }
-});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
