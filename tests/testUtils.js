9565e01cfcb6d030282c53805f546e26904aa3b4
Greg Trowbridge <gjtrowbridge@gmail.com>
2019-05-24 19:02:05 -0700

    Update clearDatabase to use async/await logic.

--- tests/testUtils.js
+++ tests/testUtils.js
@@ -1,19 +1,18 @@
 const db = require('../server/database/models/index.js');
 
 const clearDatabase = (t) => {
-  let promise = Promise.resolve(0);
-  Object.entries(db.models).forEach(([key, value]) => {
-    promise = promise.then(() => {
-      return value.destroy({
+  try {
+    const deletionPromises = Object.entries(db.models).map(async ([key, model]) => {
+      const numDeleted = await model.destroy({
         where: {},
       });
+      console.log(`successfully deleted ${numDeleted} records of model "${key}"`);
     });
-  });
-  return promise.catch((err) => {
+    return Promise.all(deletionPromises);
+  } catch (e) {
     t.fail(`unable to clear database: "${err}"`);
-  });
+  }
 };
-
 
 module.exports = {
   clearDatabase,
