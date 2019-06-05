7d8f49befa839317ce0ae516d1f804acef144c45
Greg Trowbridge <gjtrowbridge@gmail.com>
2019-06-05 13:41:10 -0700

    Remove normalize.css and stylus watch-css commands

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -16,7 +16,6 @@ import Modal from '../Modal/Modal.jsx';
 import Footer from '../Footer/Footer.jsx';
 import AreaDisabler from '../AreaDisabler/AreaDisabler.jsx';
 
-require('normalize.css/normalize.css');
 require('Components/App/App.scss');
 
 class WikiPageWrapper extends React.Component {
